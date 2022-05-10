d94427c913d348f0c9115e69ded9a180fcc71ee2
Ashish <acpawar_b18@it.vjti.ac.in>
2022-05-10 15:11:25 +0530

    updated addtopic component with alert

--- pages/AddTopic.js
+++ pages/AddTopic.js
@@ -1,15 +1,17 @@
+import { useState } from "react";
 import Form from "react-bootstrap/Form";
-import { Card } from "react-bootstrap";
+import { Alert, Card, Col, Row, Toast } from "react-bootstrap";
 import { Button } from "react-bootstrap";
 import styles from "../styles/AddTopic.module.css";
 
 export default function AddTopic() {
+  const [show, setShow] = useState(false);
+
   const handleSubmit = async (event) => {
     event.preventDefault();
 
     const form = new FormData(event.target);
     const formData = Object.fromEntries(form.entries());
-    console.log(formData);
 
     const res = await fetch("/api/addTopic", {
       body: JSON.stringify(formData),
@@ -19,36 +21,55 @@ export default function AddTopic() {
       method: "POST",
     });
     const result = await res.json();
-    console.log(result);
+    setShow(true);
   };
   return (
-    <div className={styles.container}>
-      <Card>
-        <Card.Body>
-          <div className={styles.box}>
-            <h3 className={styles.cardTitle}>Add topic information</h3>
-          </div>
-          <Form onSubmit={handleSubmit} className={styles.form}>
-            <Form.Label className={styles.topmargin}>Topic Name</Form.Label>
-            <Form.Control type="text" name="topicName" />
-            <Form.Label className={styles.topmargin}>Topic Type</Form.Label>
-            <Form.Control type="text" name="topicType" />
-            <Form.Label className={styles.topmargin}>
-              Topic Description
-            </Form.Label>
-            <Form.Control type="text" name="topicDescription" />
-            <Button
-              variant="primary"
-              size="lg"
-              type="submit"
-              className={styles.btn}
-            >
-              Add
-            </Button>
-            <div className={styles.helptext}>Fastest data storage on REDIS</div>
-          </Form>
-        </Card.Body>
-      </Card>
-    </div>
+    <>
+      <div className={styles.container}>
+        <Card>
+          <Card.Body>
+            <div>
+              {show && (
+                <Alert
+                  variant="success"
+                  onClose={() => setShow(false)}
+                  dismissible
+                >
+                  ✅ Topic has been added to Database !
+                </Alert>
+              )}
+            </div>
+            <div className={styles.box}>
+              <h3 className={styles.cardTitle}>Add Topic</h3>
+            </div>
+            <Form onSubmit={handleSubmit} className={styles.form}>
+              <Form.Label className={styles.topmargin}>
+                📌 &nbsp; Topic Name
+              </Form.Label>
+              <Form.Control type="text" name="topicName" />
+              <Form.Label className={styles.topmargin}>
+                📌 &nbsp;Topic Type
+              </Form.Label>
+              <Form.Control type="text" name="topicType" />
+              <Form.Label className={styles.topmargin}>
+                📌&nbsp;Topic Description
+              </Form.Label>
+              <Form.Control type="text" name="topicDescription" />
+              <Button
+                variant="primary"
+                size="lg"
+                type="submit"
+                className={styles.btn}
+              >
+                Add
+              </Button>
+              <div className={styles.helptext}>
+                Fastest data storage on REDIS 📙
+              </div>
+            </Form>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
   );
 }
